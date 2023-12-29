eecd96a1387c011cbfa0f67366c999bf5e994a7b
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2023-12-29 18:47:32 +0900

    Initial commit

---
+++ src/main/java/com/ppvp/PaymentProject/Controller/MainController.java
@@ -0,0 +1,30 @@
+package com.ppvp.PaymentProject.Controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @GetMapping(path = {"/",""})
+    public String mainPage(Model model) {
+        model.addAttribute("mainMessage", "안녕하세요");
+        return "hello";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
+    @GetMapping("/product")
+    public String productPage() {
+        return "product";
+    }
+
+
+}
