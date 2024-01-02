550decf3eeb1ccb32ab388e76fa1d7f4b1191cdc
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2024-01-02 16:58:13 +0900

    방화벽 문제

--- src/main/java/com/ppvp/PaymentProject/Controller/MainController.java
+++ src/main/java/com/ppvp/PaymentProject/Controller/MainController.java
@@ -5,12 +5,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @Slf4j
 public class MainController {
 
-    @GetMapping(path = {"/",""})
+    @GetMapping(path = {"/", ""})
     public String mainPage(Model model) {
         model.addAttribute("mainMessage", "안녕하세요");
         return "hello";
