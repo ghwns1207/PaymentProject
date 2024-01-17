9c98644436ca34c1a355cd150b79f78cefe5335f
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2024-01-17 13:32:11 +0900

    카카오페이

---
+++ src/main/java/com/ppvp/PaymentProject/captcha/utils/CaptchaUtil.java
@@ -0,0 +1,33 @@
+//package com.ppvp.PaymentProject.captcha.utils;
+//
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+//
+//public class CaptchaUtil {
+//
+//  private final int HEIGHT = 200;
+//
+//  private final int WIDTH = 50;
+//
+//  public void captcaImg(HttpServletRequest request, HttpServletResponse response) {
+//    Captcha captcha = new Captcha.Builder(200, 60)
+//        .addText(new NumbersAnswerProducer(6))
+//        .addNoise().addNoise().addNoise()
+//        .addBackground(new GradiatedBackgroundProducer())
+//        .addBorder()
+//        .build();
+//
+////        response.setHeader("Cache-Control", "no-cache");
+////        response.setDateHeader("Expires", 0);
+////        response.setHeader("Pragma", "no-cache");
+////        response.setDateHeader("Max-Age", 0);
+//
+//    request.getSession().setAttribute(Captcha.NAME, captcha);
+//    CaptchaServletUtil.writeImage(response, captcha.getImage());
+//
+//  }
+//
+//
+//
+//}
